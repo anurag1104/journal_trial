46d4d8fc26faa685f35e1ed7dcb20fa00cb41953
anurag1104 <anurag@sharma.ac>
2025-07-02 18:24:57 +0530

    added bcrypt password hashing session to be added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import pg from "pg";
 import {fileURLToPath} from "url";
 import path from "path";
 import dayjs from "dayjs";
+import bcrypt from "bcrypt";
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
@@ -46,29 +47,51 @@ app.post("/login", async(req,res) => {
     let password = req.body.password;
     console.log(eMail, password);
     let users = await db.query("SELECT * FROM users_login WHERE user_name = $1",[eMail]);
+
     if(users.rows.length >= 1){
-        if(users.rows[0].password == password){
-            res.redirect("/")
+        const storedHash = users.rows[0].password;
+        const pMatch = await bcrypt.compare(password, storedHash);
+        console.log(pMatch);
+        if(pMatch){
+            let entries = await db.query("SELECT u.user_name, j.title, j.content, j.created_at, j.id FROM journal_entries j INNER JOIN users_login u ON j.user_id = u.id WHERE u.user_name = $1",[eMail]);
+            entries.rows.map( (elem) => {
+                let timeStamp = elem.created_at;
+                let formatted = dayjs(timeStamp).format("DD-MM-YYYY  HH:mm");
+                elem.created_at = formatted
+                }  )
+            res.render("entries.ejs",{jentries:entries.rows});
         }
         else{
-            res.sendStatus(400);
+            res.send("Incorrect Password")
         }
     }
     else{
-
-    }
-
-})
-
-app.get("/", async(req,res) => {
-   let entries = await db.query("SELECT u.user_name, j.title, j.content, j.created_at, j.id FROM journal_entries j INNER JOIN users_login u ON j.user_id = u.id WHERE u.user_name = $1",[eMail]);
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, async(error, hash) =>{
+            if(error){
+                res.send("Error signing up");
+                console.log(error);
+            }
+            else{
+                const result = await db.query("INSERT INTO users_login(user_name,password) VALUES ($1,$2)",[eMail, hash])
+                console.log(result);
+                let entries = await db.query("SELECT u.user_name, j.title, j.content, j.created_at, j.id FROM journal_entries j INNER JOIN users_login u ON j.user_id = u.id WHERE u.user_name = $1",[eMail]);
             entries.rows.map( (elem) => {
                 let timeStamp = elem.created_at;
                 let formatted = dayjs(timeStamp).format("DD-MM-YYYY  HH:mm");
                 elem.created_at = formatted
                 }  )
                 console.log(entries.rows)
-            res.render("entries.ejs",{jentries:entries.rows});  
+                res.render("entries.ejs",{jentries:entries.rows});
+            }
+        } )
+
+    }
+
+})
+
+app.get("/", async(req,res) => {
+    
 })
 
 
